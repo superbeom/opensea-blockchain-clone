3d75bc43ea129eeb03a13d6d36f7a81153abb0eb
superbeom <it.super.beom@gmail.com>
2022-05-13 05:46:04 +0900

    Add SanityClient and thirdweb

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,60 @@
+import React, { useEffect } from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+
+import { client } from "../lib/sanityClient";
+
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+};
+
 const Home = () => {
+  // Auth
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+  // ---
+
+  useEffect(() => {
+    if (!address) return;
+
+    (async () => {
+      const userDoc = {
+        _type: "users",
+        _id: address,
+        userName: "Unnamed",
+        walletAddress: address,
+      };
+
+      const result = await client.createIfNotExists(userDoc);
+      console.log("result: ", result);
+    })();
+  }, [address]);
+
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+    <div className={style.wrapper}>
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <div className={style.walletConnectWrapper}>
+          <button className={style.button} onClick={connectWithMetamask}>
+            Connect Wallet
+          </button>
+
+          <div className={style.details}>
+            You need Chrome to be <br /> able to run this app.
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
