cf166b39da35b0b78f75ec053d234b7782ac8a9b
superbeom <it.super.beom@gmail.com>
2022-05-12 15:12:29 +0900

    Create Hero component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
